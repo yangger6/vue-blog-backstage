fe9de2b0190c8f6a4821ddb6214f961874edfa94
yangger <18902833810@163.com>
2017-06-07 21:35:57 +0800

    "第一次上传文件"

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "standard",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "no-return-assign": 2
+    }
+};
